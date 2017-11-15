1ced99179f011962daa4be83b12e2f5010aaf270
ConDawg <31518714+ConDawg@users.noreply.github.com>
2017-11-15 12:50:18 -0700

    Added all imports and methods to my ChatPanel class

--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -1,6 +1,40 @@
 package chat.view;
 
-public class ChatPanel
-{
 
+import javax.swing.SpringLayout;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import java.awt.Color;
+import chat.controller.ChatController;
+
+
+public class ChatPanel extends JPanel
+{
+	private ChatbotController appController;
+	private JButton chatButton;
+	private JTextField inputField;
+	private JTextArea chatArea;
+	private SpringLayout appLayout
+	
+	public ChatPanel(ChatController appController)
+	{
+		
+	}
+
+	private void setupPanel()
+	{
+		
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
 }
