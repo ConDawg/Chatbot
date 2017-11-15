21a7ecae2a2f777f549d5b1ee13874532e9a32c0
ConDawg <31518714+ConDawg@users.noreply.github.com>
2017-11-15 12:22:53 -0700

    Commented out errors I had with 'Panel'

--- src/chat/view/ChatFrame.java
+++ src/chat/view/ChatFrame.java
@@ -1,6 +1,26 @@
 package chat.view;
 
-public class ChatFrame
-{
+import chat.controller.ChatController;
+import javax.swing.JFrame;
 
+public class ChatFrame extends JFrame
+{
+	private ChatController appController;
+//	private ChatPanel appPanel;
+	public ChatFrame(ChatController appController)
+	{
+		super();
+		this.appController = appController;
+//		appPanel = new ChatPanel(appController);
+		setupFrame();
+	}
+	
+	private void setupFrame()
+	{
+//		this.setContentPane(appPanel);
+		this.setTitle("Chatting with BMO");
+		this.setSize(600, 600);
+		this.setVisible(true);
+		
+	}
 }
